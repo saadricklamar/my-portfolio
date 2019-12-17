79122e49b186d3b6bf9267649ed1b4ccce80badd
Saad Baradan <saadbaradan@gmail.com>
2019-12-17 15:36:15 -0700

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Hello, I'm <span>Saad Baradan.</span>
+          <br />
+          I'm a full-stack web developer.
+        </h1>
+        <button className="home-button">View my work</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
