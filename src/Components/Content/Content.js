d146042a4f6c66d87e1e3fd5e8b8acd553f3ecb6
Saad Baradan <saadbaradan@gmail.com>
2019-12-18 17:10:50 -0700

    Add img, style as Rhombus, and add description about me

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -16,7 +16,37 @@ function Content() {
         <button className="nav-buttons">Projects</button>
         <button className="nav-buttons">Contact</button>
       </header>
-      <main></main>
+      <main>
+        <section>
+        <div class="rhombus-outter">
+          <div class="rhombus">
+            <img
+              src={require("../../me.jpg")}
+              alt="me at Niagara Falls"
+              width="255"
+              height="320"
+            />
+          </div>
+        </div>
+        <h2>About Me</h2>
+        <p>
+          I'm a full-stack web developer with a background in teaching and
+          client management. After teaching all things philosophy, I decided to
+          put my passion for theoretical logic to work. I enrolled in the
+          longest and most challenging bootcamp west of the Mississippi, and
+          after 7 months of hard work, I'm loving building full-stack web
+          applications.
+          <br />
+          <br /> As a Colorado Native, I enjoy hiking, traveling, and
+          photography. When I'm not out exploring, you can find me reading,
+          writing fiction, watching the NBA, doing puzzles, and walking my dog,
+          Sophie.
+        </p>
+        </section>
+        <setion>
+        </setion>
+      </main>
+      
     </div>
   );
 }
