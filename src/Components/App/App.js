a97db6f3beb8b1d10b4378a256cd3a9e9b4701de
Saad Baradan <saadbaradan@gmail.com>
2019-12-19 16:26:03 -0700

    Install text-loop library and add different greetings

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,27 @@ import React from "react";
 import "./App.scss";
 import Content from "../Content/Content";
 import { Link, animateScroll as scroll } from "react-scroll";
-
+import TextLoop from "react-text-loop";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>
-          Hello, I'm <span>Saad Baradan.</span>
+          <TextLoop
+            children={[
+              "Hello",
+              "مرحبا",
+              "Bonjour",
+              "Hola",
+              "你好",
+              "Sveiki",
+              "नमस्ते",
+              "Hej"
+            ]}
+            interval={2000}
+          />
+          , I'm <span>Saad Baradan.</span>
           <br />
           I'm a full-stack web developer.
         </h1>
