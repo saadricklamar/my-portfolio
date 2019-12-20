aede7e63b60eba3fdd214eb949eb2d757d5f1d18
Saad Baradan <saadbaradan@gmail.com>
2019-12-20 13:16:09 -0700

    Add html/css skills as second row

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -46,6 +46,64 @@ function Skills() {
             </section>
           </section>
           <hr />
+          <section className="skills">
+            <h5>HTML/CSS</h5>
+            <section className="skill">
+              <a
+                href="https://html.spec.whatwg.org/#is-this-html5?"
+                target="_blank"
+              >
+                <img
+                  src={require("../../assets/html5.svg")}
+                  alt="HTML 5 logo"
+                  width="45"
+                  height="45"
+                  className="logo"
+                />
+              </a>
+              <p className="p-two">HTML5</p>
+            </section>
+            <section className="skill">
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Archive/CSS3"
+                target="_blank"
+              >
+                <img
+                  src={require("../../assets/css3.svg")}
+                  alt="CSS 3 logo"
+                  width="45"
+                  height="45"
+                  className="logo"
+                />
+              </a>
+              <p className="p-two">CSS3</p>
+            </section>
+            <section className="skill">
+              <a href="https://getbootstrap.com/" target="_blank">
+                <img
+                  src={require("../../assets/bootstrap.svg")}
+                  alt="Bootstrap logo"
+                  width="45"
+                  height="45"
+                  className="logo"
+                />
+              </a>
+              <p className="p-two">Bootstrap</p>
+            </section>
+            <section className="skill">
+              <a href="https://sass-lang.com/" target="_blank">
+                <img
+                  src={require("../../assets/sass.svg")}
+                  alt="Sass logo"
+                  width="45"
+                  height="45"
+                  className="logo"
+                />
+              </a>
+              <p className="p-two">Sass</p>
+            </section>
+          </section>
+          <hr />
         </article>
       </main>
     </div>
