d4130494b6e067e6ea30722cc17bff26abbb3b66
Saad Baradan <saadbaradan@gmail.com>
2019-12-25 13:36:33 -0700

    Add contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Contact.scss";
 
-function Contact() {
-  return (
-    <main className="contact-page">
-      <p>hi</p>
-    </main>
-  );
+export class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <main className="contact-page">
+        <form className="contact-form">
+          <h3>Want to work together or have a question?</h3>
+          <input className="name" placeholder="Name"></input>
+          <input className="email" placeholder="Email"></input>
+          <textarea
+            className="message"
+            placeholder="Enter Your Message"
+          ></textarea>
+          <button className="submit">Submit</button>
+        </form>
+      </main>
+    );
+  }
 }
 
 export default Contact;
