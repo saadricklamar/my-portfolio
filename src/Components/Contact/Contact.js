3aea2c99da9ea278095a5de4b925b72b3dc3929f
Saad Baradan <saadbaradan@gmail.com>
2019-12-25 12:28:26 -0700

    Add contact page component

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.scss";
+
+function Contact() {
+  return (
+    <main className="contact-page">
+      <p>hi</p>
+    </main>
+  );
+}
+
+export default Contact;
