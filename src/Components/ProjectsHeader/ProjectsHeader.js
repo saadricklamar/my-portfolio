6c11686726e749dcd996a50e19143b63c98f936d
Saad Baradan <saadbaradan@gmail.com>
2019-12-25 11:30:27 -0700

    Add projects header component

---
+++ src/Components/ProjectsHeader/ProjectsHeader.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./ProjectsHeader.scss";
+
+function ProjectsHeader(props) {
+  return (
+    <header className="projects-header">
+      <button className="project-buttons" onClick={props.renderAll}>
+        All
+      </button>
+      <button className="project-buttons" onClick={props.renderReact}>
+        React
+      </button>
+      <button className="project-buttons" onClick={props.renderVue}>
+        Vue.js
+      </button>
+      <button className="project-buttons" onClick={props.renderNode}>
+        Node.js
+      </button>
+    </header>
+  );
+}
+
+export default ProjectsHeader;
