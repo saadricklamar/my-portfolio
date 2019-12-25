c4d7c6d6dcb1d6ea81cd5e1e1fc3bd0c5dc5bd3e
Saad Baradan <saadbaradan@gmail.com>
2019-12-25 11:22:49 -0700

    Conditionally render projects on button clicks

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,115 +1,327 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Projects.scss";
 
-function Projects() {
-  return (
-    <main className="projects">
-      <header className="projects-header">
-        <button className="project-buttons">All</button>
-        <button className="project-buttons">React</button>
-        <button className="project-buttons">Vue.js</button>
-        <button className="project-buttons">Node.js</button>
-      </header>
-      <section className="projects-container">
-        <article className="project">
-          <img
-            src={require("../../assets/cycle-city.png")}
-            alt="Home page of Cycle City Project"
-            className="project-image"
-          />
-          <div className="middle">
-            <p className="text">Cycle City</p>
-            <a
-              href="https://github.com/saadricklamar/cycle-city"
-              target="_blank"
-            >
-              <button className="learn-more">Learn More</button>
-            </a>
-          </div>
-        </article>
-        <article className="project">
-          <img
-            src={require("../../assets/harvard-art.png")}
-            alt="Home page of Harvard Art Museum Project"
-            className="project-image"
-          />
-          <div className="middle">
-            <p className="text">Harvard Art Museum</p>
-            <a
-              href="https://github.com/saadricklamar/harvard-art"
-              target="_blank"
-            >
-              <button className="learn-more">Learn More</button>
-            </a>
-          </div>
-        </article>
-        <article className="project">
-          <img
-            src={require("../../assets/vue-calculator.png")}
-            alt="Home page of Vue Calculator project"
-            className="project-image"
-          />
-          <div className="middle">
-            <p className="text">Vue Calculator</p>
-            <a
-              href="https://github.com/saadricklamar/vue-calculator"
-              target="_blank"
-            >
-              <button className="learn-more">Learn More</button>
-            </a>
-          </div>
-        </article>
-        <article className="project">
-          <img
-            src={require("../../assets/movie-tracker.png")}
-            alt="Home page of Movie Tracker project"
-            className="project-image"
-          />
-          <div className="middle">
-            <p className="text">Movie Tracker</p>
-            <a
-              href="https://github.com/saadricklamar/movie-tracker"
-              target="_blank"
-            >
-              <button className="learn-more">Learn More</button>
-            </a>
-          </div>
-        </article>
-        <article className="project">
-          <img
-            src={require("../../assets/shakesbeer.png")}
-            alt="Home page of Shakesbeer project"
-            className="project-image"
-          />
-          <div className="middle">
-            <p className="text">ShakesBeer</p>
-            <a
-              href="https://github.com/saadricklamar/shakesbeer"
-              target="_blank"
-            >
-              <button className="learn-more">Learn More</button>
-            </a>
-          </div>
-        </article>
-        <article className="project">
-          <img
-            src={require("../../assets/philosophers.png")}
-            alt="Philosophers Restful API"
-            className="project-image"
-          />
-          <div className="middle">
-            <p className="text">Philosophers Back-End</p>
-            <a
-              href="https://github.com/saadricklamar/philosophers-backend"
-              target="_blank"
-            >
-              <button className="learn-more">Learn More</button>
-            </a>
-          </div>
-        </article>
-      </section>
-    </main>
-  );
+export class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      all: true,
+      react: false,
+      vue: false,
+      node: false
+    };
+  }
+
+  renderAll = () => {
+    this.setState({ vue: false });
+    this.setState({ node: false });
+    this.setState({ react: false });
+    this.setState({ all: true });
+  };
+
+  renderReact = () => {
+    this.setState({ all: false });
+    this.setState({ vue: false });
+    this.setState({ node: false });
+    this.setState({ react: true });
+  };
+
+  renderVue = () => {
+    this.setState({ all: false });
+    this.setState({ react: false });
+    this.setState({ node: false });
+    this.setState({ vue: true });
+  };
+
+  renderNode = () => {
+    this.setState({ all: false });
+    this.setState({ react: false });
+    this.setState({ vue: false });
+    this.setState({ node: true });
+  };
+
+  render() {
+    if (this.state.all) {
+      return (
+        <main className="projects">
+          <header className="projects-header">
+            <button className="project-buttons" onClick={this.renderAll}>
+              All
+            </button>
+            <button className="project-buttons" onClick={this.renderReact}>
+              React
+            </button>
+            <button className="project-buttons" onClick={this.renderVue}>
+              Vue.js
+            </button>
+            <button className="project-buttons" onClick={this.renderNode}>
+              Node.js
+            </button>
+          </header>
+          <section className="projects-container">
+            <article className="project">
+              <img
+                src={require("../../assets/cycle-city.png")}
+                alt="Home page of Cycle City Project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Cycle City</p>
+                <a
+                  href="https://github.com/saadricklamar/cycle-city"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/harvard-art.png")}
+                alt="Home page of Harvard Art Museum Project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Harvard Art Museum</p>
+                <a
+                  href="https://github.com/saadricklamar/harvard-art"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/vue-calculator.png")}
+                alt="Home page of Vue Calculator project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Vue Calculator</p>
+                <a
+                  href="https://github.com/saadricklamar/vue-calculator"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/movie-tracker.png")}
+                alt="Home page of Movie Tracker project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Movie Tracker</p>
+                <a
+                  href="https://github.com/saadricklamar/movie-tracker"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/shakesbeer.png")}
+                alt="Home page of Shakesbeer project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">ShakesBeer</p>
+                <a
+                  href="https://github.com/saadricklamar/shakesbeer"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/philosophers.png")}
+                alt="Philosophers Restful API"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Philosophers Back-End</p>
+                <a
+                  href="https://github.com/saadricklamar/philosophers-backend"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+          </section>
+        </main>
+      );
+    }
+    if (this.state.node) {
+      return (
+        <main className="projects">
+          <header className="projects-header">
+            <button className="project-buttons" onClick={this.renderAll}>
+              All
+            </button>
+            <button className="project-buttons" onClick={this.renderReact}>
+              React
+            </button>
+            <button className="project-buttons" onClick={this.renderVue}>
+              Vue.js
+            </button>
+            <button className="project-buttons" onClick={this.renderNode}>
+              Node.js
+            </button>
+          </header>
+          <section className="projects-container">
+            <article className="project">
+              <img
+                src={require("../../assets/philosophers.png")}
+                alt="Philosophers Restful API"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Philosophers Back-End</p>
+                <a
+                  href="https://github.com/saadricklamar/philosophers-backend"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+          </section>
+        </main>
+      );
+    }
+    if (this.state.react) {
+      return (
+        <main className="projects">
+          <header className="projects-header">
+            <button className="project-buttons" onClick={this.renderAll}>
+              All
+            </button>
+            <button className="project-buttons" onClick={this.renderReact}>
+              React
+            </button>
+            <button className="project-buttons" onClick={this.renderVue}>
+              Vue.js
+            </button>
+            <button className="project-buttons" onClick={this.renderNode}>
+              Node.js
+            </button>
+          </header>
+          <section className="projects-container">
+            <article className="project">
+              <img
+                src={require("../../assets/cycle-city.png")}
+                alt="Home page of Cycle City Project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Cycle City</p>
+                <a
+                  href="https://github.com/saadricklamar/cycle-city"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/movie-tracker.png")}
+                alt="Home page of Movie Tracker project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Movie Tracker</p>
+                <a
+                  href="https://github.com/saadricklamar/movie-tracker"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/shakesbeer.png")}
+                alt="Home page of Shakesbeer project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">ShakesBeer</p>
+                <a
+                  href="https://github.com/saadricklamar/shakesbeer"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+          </section>
+        </main>
+      );
+    }
+    if (this.state.vue) {
+      return (
+        <main className="projects">
+          <header className="projects-header">
+            <button className="project-buttons" onClick={this.renderAll}>
+              All
+            </button>
+            <button className="project-buttons" onClick={this.renderReact}>
+              React
+            </button>
+            <button className="project-buttons" onClick={this.renderVue}>
+              Vue.js
+            </button>
+            <button className="project-buttons" onClick={this.renderNode}>
+              Node.js
+            </button>
+          </header>
+          <section className="projects-container">
+            <article className="project">
+              <img
+                src={require("../../assets/harvard-art.png")}
+                alt="Home page of Harvard Art Museum Project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Harvard Art Museum</p>
+                <a
+                  href="https://github.com/saadricklamar/harvard-art"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+            <article className="project">
+              <img
+                src={require("../../assets/vue-calculator.png")}
+                alt="Home page of Vue Calculator project"
+                className="project-image"
+              />
+              <div className="middle">
+                <p className="text">Vue Calculator</p>
+                <a
+                  href="https://github.com/saadricklamar/vue-calculator"
+                  target="_blank"
+                >
+                  <button className="learn-more">Learn More</button>
+                </a>
+              </div>
+            </article>
+          </section>
+        </main>
+      );
+    }
+  }
 }
 
 export default Projects;
