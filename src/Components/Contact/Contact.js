e908190b6b287e87cc03f151ce5563790efb4bf6
Saad Baradan <saadbaradan@gmail.com>
2019-12-26 11:36:23 -0700

    Capture name, email, and message inputs and set to state

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,19 +4,40 @@ import "./Contact.scss";
 export class Contact extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      name: "",
+      email: "",
+      message: ""
+    };
   }
+
+  handleFormChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
       <main className="contact-page">
         <form className="contact-form">
           <h3>Want to work together or have a question?</h3>
-          <input className="name" placeholder="Name"></input>
-          <input className="email" placeholder="Email"></input>
+          <input
+            className="name"
+            name="name"
+            placeholder="Name"
+            onChange={this.handleFormChange}
+          ></input>
+          <input
+            className="email"
+            name="email"
+            placeholder="Email"
+            onChange={this.handleFormChange}
+          ></input>
           <textarea
             className="message"
             placeholder="Enter Your Message"
+            name="message"
+            onChange={this.handleFormChange}
           ></textarea>
           <button className="submit">Submit</button>
         </form>
