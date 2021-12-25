8d1e05b8937bbef6e0b3fa0642f067573b1be7e9
Saad Baradan <saadbaradan@gmail.com>
2021-12-25 11:21:54 -0700

    Fix mobile header drop down/up

--- src/Components/MobileHeader/MobileHeader.js
+++ src/Components/MobileHeader/MobileHeader.js
@@ -1,35 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MobileHeader.scss";
 import Sticky from "react-stickynode";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 function MobileHeader() {
+  let [dropDown, toggle] = useState("0px");
+
+  const handleClick = () => {
+    dropDown === "0px" ? toggle("275px") : toggle("0px");
+  };
+
   return (
     <Sticky top="#header" enabled={true} bottomBoundary="#content" innerZ={3}>
       <header className="mobile-header">
         <a href="#mobile-home" className="logo">
           <FontAwesomeIcon icon={faHome} className="home-icon" />
         </a>
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <input
+          className="menu-btn"
+          type="checkbox"
+          id="menu-btn"
+          onClick={handleClick}
+        />
         <label className="menu-icon" for="menu-btn">
           <span className="navicon"></span>
         </label>
-        <ul className="menu">
+        <ul
+          className="menu"
+          style={{ "max-height": `${dropDown}`, height: "300px" }}
+        >
           <li>
-            <a href="#mobile-about">About</a>
+            <a href="#mobile-about" onClick={handleClick}>
+              About
+            </a>
           </li>
           <li>
-            <a href="#mobile-experience">Experience</a>
+            <a href="#mobile-experience" onClick={handleClick}>
+              Experience
+            </a>
           </li>
           <li>
-            <a href="#mobile-skills">Skills</a>
+            <a href="#mobile-skills" onClick={handleClick}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#mobile-projects">Projects</a>
+            <a href="#mobile-projects" onClick={handleClick}>
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#mobile-contact">Contact</a>
+            <a href="#mobile-contact" onClick={handleClick}>
+              Contact
+            </a>
           </li>
           <li>
             <a
@@ -37,6 +61,7 @@ function MobileHeader() {
               target="_blank"
               rel="noopener noreferrer"
               id="mobile-resume"
+              onClick={handleClick}
             >
               Resume
             </a>
