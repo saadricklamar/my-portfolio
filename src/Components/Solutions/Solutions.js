71e389a6d10dbf3eff73ac9f2ae542b847a1361d
Saad Baradan <saadbaradan@gmail.com>
2024-03-31 11:54:44 -0600

    Update solutions page

--- src/Components/Solutions/Solutions.js
+++ src/Components/Solutions/Solutions.js
@@ -1,223 +1,86 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useState } from "react";
+import { solutions } from "../../data/solutionsData";
 import {
-  Main,
-  Container,
-  Card,
-  Image,
-  Overlay,
-  ProjectName,
-  TechUsed,
   Button,
+  Card,
+  CardContainer,
+  Image,
+  Main,
   Nav,
   NavButton,
+  Overlay,
+  SolutionName,
+  TechUsed,
 } from "./styles";
 
 export const Solutions = () => {
-  const [cards, setCards] = useState();
-  const [navButtons, setNavButtons] = useReducer(
-    (state, updates) => ({ ...state, ...updates }),
-    {
-      all: true,
-      loyal: false,
-      iqgeo: false,
-      imaigeAI: false,
-    }
+  const [navButtons, setNavButtons] = useState({
+    all: true,
+    imaigeAI: false,
+    iqgeo: false,
+    loyal: false,
+  });
+
+  const filteredSolutions = solutions.filter(
+    (solution) => navButtons.all || navButtons[solution.company]
   );
 
-  const careData = (
-    <Card>
-      <Image
-        src={require("../../assets/care-data.png")}
-        alt="Loyal's data management application"
-      />
-      <Overlay className="overlay">
-        <ProjectName>Care Data</ProjectName>
-        <TechUsed>TypeScript / React</TechUsed>
-        <a
-          href="https://loyalhealth.com/data-management/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>VIEW SOLUTION</Button>
-        </a>
-      </Overlay>
-    </Card>
-  );
-
-  const careSearch = (
-    <Card>
-      <Image
-        src={require("../../assets/care-search.png")}
-        alt="Loyal's care search application"
-      />
-      <Overlay className="overlay">
-        <ProjectName>Care Search</ProjectName>
-        <TechUsed>TypeScript / React</TechUsed>
-        <a
-          href="https://loyalhealth.com/care-search/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>VIEW SOLUTION</Button>
-        </a>
-      </Overlay>
-    </Card>
-  );
-
-  const inspectionSurvey = (
-    <Card>
-      <Image
-        src={require("../../assets/iqgeo-inspection-survey.png")}
-        alt="IQGeo's inspection & survey application"
-      />
-      <Overlay className="overlay">
-        <ProjectName>Inspection & Survey</ProjectName>
-        <TechUsed>JavaScript / jQuery</TechUsed>
-        <a
-          href="https://www.iqgeo.com/product/inspection-survey"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>VIEW SOLUTION</Button>
-        </a>
-      </Overlay>
-    </Card>
-  );
-
-  const workflowManager = (
-    <Card>
-      <Image
-        src={require("../../assets/workflow-manager.png")}
-        alt="IQGeo's workflow manager application."
-      />
-      <Overlay className="overlay">
-        <ProjectName>Workflow Manager</ProjectName>
-        <TechUsed>JavaScript / React</TechUsed>
-        <a
-          href="https://www.iqgeo.com/product/workflow-manager"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>VIEW SOLUTION</Button>
-        </a>
-      </Overlay>
-    </Card>
-  );
-
-  const revenueOptimizer = (
-    <Card>
-      <Image
-        src={require("../../assets/revenue-optimizer.png")}
-        alt="IQGeo's network revenue optimizer application"
-      />
-      <Overlay className="overlay">
-        <ProjectName>Network Revenue Optimizer</ProjectName>
-        <TechUsed>JavaScript / jQuery</TechUsed>
-        <a
-          href="https://www.iqgeo.com/product/network-revenue-optimizer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>VIEW SOLUTION</Button>
-        </a>
-      </Overlay>
-    </Card>
-  );
-
-  const imaigeAI = (
-    <Card>
-      <Image
-        src={require("../../assets/imaige-ai.png")}
-        alt="Imaige AI application"
-      />
-      <Overlay className="overlay">
-        <ProjectName>Photo Assistant</ProjectName>
-        <TechUsed>TypeScript / React / Electron</TechUsed>
-        <a
-          href="https://imaige.com/products/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>VIEW SOLUTION</Button>
-        </a>
-      </Overlay>
-    </Card>
-  );
-
-  useEffect(() => {
-    setCards(
-      <Container>
-        {imaigeAI}
-        {careData}
-        {careSearch}
-        {inspectionSurvey}
-        {workflowManager}
-        {revenueOptimizer}
-      </Container>
-    );
-  }, []);
-
-  const handleClick = (e) => {
-    const id = e.currentTarget.id;
-    if (id === "all") {
-      setNavButtons({ all: true, loyal: false, iqgeo: false, imaigeAI: false });
-      setCards(
-        <Container>
-          {imaigeAI}
-          {careData}
-          {careSearch}
-          {inspectionSurvey}
-          {workflowManager}
-          {revenueOptimizer}
-        </Container>
-      );
-    }
-    if (id === "loyal") {
-      setNavButtons({ all: false, loyal: true, iqgeo: false, imaigeAI: false });
-      setCards(
-        <Container>
-          {careData}
-          {careSearch}
-        </Container>
-      );
-    }
-    if (id === "iqgeo") {
-      setNavButtons({ all: false, loyal: false, iqgeo: true, imaigeAI: false });
-      setCards(
-        <Container>
-          {inspectionSurvey}
-          {workflowManager}
-          {revenueOptimizer}
-        </Container>
-      );
-    }
-    if (id === "imaige-ai") {
-      setNavButtons({ all: false, loyal: false, iqgeo: false, imaigeAI: true });
-      setCards(<Container>{imaigeAI}</Container>);
-    }
+  const handleClick = (id) => {
+    setNavButtons((prevNavButtons) => ({
+      ...prevNavButtons,
+      all: id === "all",
+      imaigeAI: id === "imaigeAI",
+      iqgeo: id === "iqgeo",
+      loyal: id === "loyal",
+    }));
   };
 
   return (
     <Main className="solutions">
       <Nav>
-        <NavButton id="all" onClick={handleClick} active={navButtons.all}>
+        <NavButton
+          id="all"
+          onClick={() => handleClick("all")}
+          active={navButtons.all}
+        >
           All
         </NavButton>
         <NavButton
-          id="imaige-ai"
-          onClick={handleClick}
+          id="imaigeAI"
+          onClick={() => handleClick("imaigeAI")}
           active={navButtons.imaigeAI}
         >
           Imaige
         </NavButton>
-        <NavButton id="loyal" onClick={handleClick} active={navButtons.loyal}>
+        <NavButton
+          id="loyal"
+          onClick={() => handleClick("loyal")}
+          active={navButtons.loyal}
+        >
           Loyal
         </NavButton>
-        <NavButton id="iqgeo" onClick={handleClick} active={navButtons.iqgeo}>
+        <NavButton
+          id="iqgeo"
+          onClick={() => handleClick("iqgeo")}
+          active={navButtons.iqgeo}
+        >
           IQGeo
         </NavButton>
       </Nav>
-      {cards}
+      <CardContainer>
+        {filteredSolutions.map((solution) => (
+          <Card key={solution.name}>
+            <Image src={solution.imgSrc} alt={solution.alt} />
+            <Overlay className="overlay">
+              <SolutionName>{solution.name}</SolutionName>
+              <TechUsed>{solution.techUsed}</TechUsed>
+              <a href={solution.link} target="_blank" rel="noopener noreferrer">
+                <Button>VIEW SOLUTION</Button>
+              </a>
+            </Overlay>
+          </Card>
+        ))}
+      </CardContainer>
     </Main>
   );
 };
